a29655b9d7e0eb4895e55e6e46f0fb9760a0ffa9
ElineSophie <soof1994@gmail.com>
2019-05-02 14:22:18 +0200

    Final version

--- Homework/Week_3/myScript.js
+++ Homework/Week_3/myScript.js
@@ -7,42 +7,73 @@ var txtFile = new XMLHttpRequest();
 
 txtFile.onreadystatechange = function() {
   if (txtFile.readyState === 4 && txtFile.status == 200) {
-    console.log(JSON.parse(txtFile.responseText));
+    var data = JSON.parse(txtFile.responseText);
 
-    var x = createTransform([2005, 2017], [50,500]);
-    var y = createTransform([800, 2500], [300, 100]);;
-
-    // Store JSON data
-    data = JSON.parse(txtFile.responseText);
+    // Transformation
+    var x = createTransform([2005, 2017], [100, 600]);
+    var y = createTransform([800, 2400], [300, 100]);;
 
     // Make canvas lines
     const canvas = document.getElementById('canvas');
     const ctx = canvas.getContext('2d');
 
+    ctx.moveTo(100, 300);
+    ctx.lineTo(100, 100);
+    ctx.moveTo(100, 300);
+    ctx.lineTo(600, 300);
+    ctx.stroke();
+
+    ctx.moveTo(x(Number(data[0]["Year"])), y((Number(data[0]["Total Burnout Rate"]))));
+
+    // Create list for ticks
+    yTicks = [y(800), y(2400)];
+    console.log(yTicks);
+    xTicks = [];
+
     data.forEach(function(d){
       // Get numeric of year and burnout
-      d["Year"] = Number(d["Year"])
-      d["Total Burnout Rate"] = Number(d["Total Burnout Rate"])
+      d["Year"] = Number(d["Year"]);
+      d["Total Burnout Rate"] = Number(d["Total Burnout Rate"]);
+      console.log(d["Total Burnout Rate"]);
+      xTicks.push(x(d["Year"]));
       // Draw line to each point
       ctx.lineTo(x(d["Year"]), y(d["Total Burnout Rate"]));
       });
+    console.log(xTicks);
 
-      // Plot the lines
-      ctx.stroke();
+    ctx.moveTo(100, 310);
 
-    ctx.moveTo(50, 500);
-    ctx.lineTo(50, 50);
-    ctx.moveTo(50, 500);
-    ctx.lineTo(500, 500);
+    // Calculate difference between ticks
+    nextX = xTicks[1] - xTicks[0];
+
+    // Make ticks
+    xTicks.forEach(function(xTick, index){
+      ctx.lineTo(xTick, 300);
+      ctx.moveTo(xTick + nextX, 310);
+      ctx.fillText(data[index]["Year"], xTick - 10, 325);
+    })
+
+    // Draw ticks for y axis
+    for (i = 800; i <= 2400; i += 200){
+      yAxis = y(i);
+      ctx.moveTo(90, y(i));
+      ctx.lineTo(100, y(i));
+      ctx.fillText(i, 65, y(i));
+    };
+
+    // Plot the lines
     ctx.stroke();
 
-    // Text for the graph itself
+    // Title
     ctx.font = "20px serif";
-    ctx.fillText('Meldingen van burn-out als beroepziekte 2005 tot 2017',100,50);
-    ctx.fillText('Jaar',425,550);
-    ctx.rotate(-90 * Math.PI / 180)
-    ctx.translate(-100,450)
-    ctx.fillText('Aantal meldingen',-200,-420);
+    ctx.fillText('Meldingen van burn-out als beroepziekte 2005 tot 2017',120,60);
+
+    // Text for x and y
+    ctx.font = "15px serif";
+    ctx.fillText('Jaar',550,350);
+    ctx.rotate(-90 * Math.PI / 180);
+    ctx.translate(-100,450);
+    ctx.fillText('Aantal meldingen',-150,-400);
   }
 }
 txtFile.open("GET", fileName);
